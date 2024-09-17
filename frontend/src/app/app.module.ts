17abdc23927ed43bf992c943124cf47e87bb3200
javii <javiisalaas97@gmail.com>
2024-09-17 21:12:54 +0200

    frontend init with footer

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FooterComponent } from './components/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+
+})
+export class AppModule { }
