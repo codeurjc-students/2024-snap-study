3c8d5cb74277d54c9d325cd6475a40a6b2a73cf3
Javi <102818127+jrodriguezs2020@users.noreply.github.com>
2024-11-10 19:42:59 +0100

    new main section

--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Degree } from '../../models/degree.model';
+import { DegreeService } from '../../services/degree.service';
 
 @Component({
   selector: 'app-main',
@@ -10,8 +12,37 @@ export class MainComponent {
   title = 'SnapStudy';
 
   public searchText: string = '';
+  public degrees: Degree[];
 
-  constructor(private router: Router){}
+  public indexdegrees: number = 0;    //ajax
+  public moredegrees: boolean = false;   //ajax
+
+  constructor(private router: Router, private degreeService: DegreeService){
+    this.degrees = [];
+  }
+
+  ngOnInit() {
+    this.degreeService.getDegrees(this.indexdegrees).subscribe((response) => {
+      this.degrees = this.degrees.concat(response.content);
+      this.moredegrees = !response.last;
+      this.indexdegrees++; //next ajax buttom
+    });
+  }
+
+  getMoredegrees() {
+    this.degreeService.getDegrees(this.indexdegrees).subscribe((response) => {
+        this.degrees = this.degrees.concat(response.content);
+        this.moredegrees = !response.last;
+        this.indexdegrees++;
+      });
+  }
+
+  showLoadMoreButton(){
+    if (this.moredegrees){
+      return true;
+    }
+    return false;
+  }
 
   search(){
     this.router.navigate(['/search', this.searchText]);
