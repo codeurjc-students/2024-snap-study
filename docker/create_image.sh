388d5636f01e473c7d633fdd48e4f1365da655d5
Javi <102818127+jrodriguezs2020@users.noreply.github.com>
2024-11-25 20:03:31 +0100

    docker and SPA Controller

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,6 +1,6 @@
 #sets build context to parent and builds using Dockerfile from docker directory
-docker build -t jrodriguezs2020/snapstudy2 -f docker/Dockerfile .
+docker build -t jrodriguezs2020/snapstudy -f docker/Dockerfile .
 #Pushes the image to dockerhub
-docker push jrodriguezs2020/snapstudy2
+docker push jrodriguezs2020/snapstudy
 #Run the image using docker-compose from docker directory
 docker-compose up
