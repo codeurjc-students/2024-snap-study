2890521af308a7bea447b75cac8c44182085e038
Javi <102818127+jrodriguezs2020@users.noreply.github.com>
2025-01-13 19:07:43 +0100

    fix database ids

--- backend/src/test/java/com/snapstudy/backend/tests/api/DownloadDocumentApiTest.java
+++ backend/src/test/java/com/snapstudy/backend/tests/api/DownloadDocumentApiTest.java
@@ -4,12 +4,17 @@ import java.util.Map;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.snapstudy.backend.service.DegreeService;
+import com.snapstudy.backend.service.DocumentService;
+
 import static org.hamcrest.Matchers.*;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
 
 public class DownloadDocumentApiTest {
 
+    private DocumentService documentService;
     private static Map<String, String> cookies;
     private static LoginApiTestService loginApiTestService;
     private static String API_URL = "https://localhost:8443/api/documents/download/{id}";
@@ -25,9 +30,11 @@ public class DownloadDocumentApiTest {
     @Test
     public void testDownloadDocument_Success() {
 
+        Long documentId = documentService.getDocumentByName("mia").getId();
+
         given()
                 .cookies(cookies)
-                .pathParam("id", 6) // Valid document ID
+                .pathParam("id", documentId) // Valid document ID
                 .when()
                 .get(API_URL)
                 .then()
