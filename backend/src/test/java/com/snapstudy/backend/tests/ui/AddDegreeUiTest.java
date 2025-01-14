0ad87cbf32a53b1f5553f5d2ba5306c3bc4ca0ce
Javi <102818127+jrodriguezs2020@users.noreply.github.com>
2025-01-14 18:30:17 +0100

    disable passwords alerts

--- backend/src/test/java/com/snapstudy/backend/tests/ui/AddDegreeUiTest.java
+++ backend/src/test/java/com/snapstudy/backend/tests/ui/AddDegreeUiTest.java
@@ -33,6 +33,8 @@ public class AddDegreeUiTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--headless");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-popup-blocking");
         options.setAcceptInsecureCerts(true);
         driver = new ChromeDriver(options);
     }
